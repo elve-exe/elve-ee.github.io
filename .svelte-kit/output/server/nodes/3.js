1d2379fd18e29ee4c725006daf06e943ed27dfd7
elve-exe <elve.exe@gmail.com>
2024-10-08 13:20:40 +0200

    Revert "z"
    This reverts commit 1d69e05ff709d9de55f2c9f0678e40da2b756137.

--- .svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/about/_page.svelte.js')).default;
-export const imports = ["app/immutable/nodes/3.BHy6IQHk.js","app/immutable/chunks/scheduler.Bmg8oFKD.js","app/immutable/chunks/index.CpO4BYFI.js"];
-export const stylesheets = ["app/immutable/assets/3.6zUY_dsb.css"];
-export const fonts = [];
